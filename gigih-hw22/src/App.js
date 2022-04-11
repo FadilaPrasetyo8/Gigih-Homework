29bbf41d9ce3ca62806842165c45f4775180120f
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2022-04-11 09:13:00 +0700

    update homework 4.3

--- gigih-hw22/src/App.js
+++ gigih-hw22/src/App.js
@@ -2,12 +2,51 @@ import "./App.css";
 // import Main from "./pages/home/main";
 // import Card from "./components/Card/card";
 import Main from "./pages/home/Index";
+// import KodinganLama from "./components/kodinganLama";
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import { getToken } from "./auth/auth";
+import LoginPage from "./pages/Login/Login";
+import TokenContext from "./contex/contex";
 
 function App() {
+  const [token, setToken] = useState("");
+  const value = useMemo(() => ({ token, setToken }), [token]);
+
+  // Check if the token is available
+  useEffect(() => {
+    if (!token) {
+      setToken(getToken());
+    }
+  }, []);
+
   return (
-    <div>
-      <Main />
-    </div>
+    <TokenContext.Provider value={value}>
+      <Router>
+        <Switch>
+          <Route path="/create-playlist">
+            {!token ? (
+              <Redirect exact from="/create-playlist" to="/" />
+            ) : (
+              <Main />
+            )}
+          </Route>
+          <Route path="/">
+            {token ? (
+              <Redirect exact from="/" to="/create-playlist" />
+            ) : (
+              <LoginPage />
+            )}
+          </Route>
+        </Switch>
+      </Router>
+    </TokenContext.Provider>
   );
 }
 
